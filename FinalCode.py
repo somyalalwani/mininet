6caac39ea7524ec59872342416a5400df94d45c9
sanyamjain335 <sanyamjain335@gmail.com>
2021-10-18 17:32:17 +0530

    Connection request fixed

--- FinalCode.py
+++ FinalCode.py
@@ -379,12 +379,12 @@ G = nx.Graph()
 try:
 
     #Enables Statistics Like B/W, etc
-    enableStats = "http://localhost:8080/wm/statistics/config/enable/json"
-    requests.put(enableStats)
+    # enableStats = "http://localhost:8080/wm/statistics/config/enable/json"
+    # requests.put(enableStats)
 
     #Device Info (Switch To Which The Device Is Connected & The MAC Address Of Each Device)
-    deviceInfo = "http://localhost:8080/wm/device/"    
-    fetchResponse(deviceInfo,"deviceInfo")
+    # deviceInfo = "http://localhost:8080/wm/device/"    
+    # fetchResponse(deviceInfo,"deviceInfo")
 
     loadbalance()
     os.system('clear')
