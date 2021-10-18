e0a7d81da80682cf89daf48fa76ee16e58ada0a1
sanyamjain335 <sanyamjain335@gmail.com>
2021-10-18 18:03:01 +0530

    Changes 2

--- FinalCode.py
+++ FinalCode.py
@@ -383,7 +383,7 @@ try:
     requests.put(enableStats)
 
     #Device Info (Switch To Which The Device Is Connected & The MAC Address Of Each Device)
-    deviceInfo = "http://localhost:3022/wm/device/"    
+    deviceInfo = "http://localhost:8080/wm/device/"    
     fetchResponse(deviceInfo,"deviceInfo")
 
     loadbalance()
